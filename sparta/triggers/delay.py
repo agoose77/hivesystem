c2916994c4439e87d596a0d70987d43ee8e0c971
agoose77 <goosey15@gmail.com>
2014-06-21 22:52:38 +0100

    Implemented delay trigger

--- sparta/triggers/delay.py
+++ sparta/triggers/delay.py
@@ -1,4 +1,6 @@
 import libcontext, bee
+from libcontext.socketclasses import *
+from libcontext.pluginclasses import *
 from bee.segments import *
 
 
@@ -25,16 +27,29 @@ class delay(object):
         class delay(bee.worker):
             __doc__ = cls.__doc__
 
-            trig = output("push", "trigger")
-            trigfunc = triggerfunc(trig)
+            trig = antenna("push", "trigger")
+
+            trig_deferred = output("push", "trigger")
+            trigfunc = triggerfunc(trig_deferred)
 
             if mode == "frames":
                 delay = antenna("pull", "int")
                 b_delay = buffer("pull", "int")
+
+                @property
+                def time_value(self):
+                    return self.pacemaker.ticks
+
             elif mode == "seconds":
                 delay = antenna("pull", "float")
                 b_delay = buffer("pull", "float")
+
+                @property
+                def time_value(self):
+                    return self.pacemaker.time
+
             connect(delay, b_delay)
+            trigger(trig, b_delay)
 
             multi = variable("bool")
             parameter(multi, False)
@@ -50,7 +65,43 @@ class delay(object):
                 f.multi.name = "Multi mode"
                 f.multi.advanced = True
 
+            @modifier
+            def add_callback(self):
+                self.pending_triggers.append(self.time_value + self.b_delay)
+
+            trigger(b_delay, add_callback)
+
+            def update_triggers(self):
+                """Check for any valid triggers and run them"""
+                time_value = self.time_value
+                pending_times = self.pending_triggers[:]
+
+                for target_time in pending_times:
+                    if target_time > time_value:
+                        continue
+
+                    self.pending_triggers.remove(target_time)
+                    self.trigfunc()
+
+            def set_add_listener(self, add_listener):
+                self.add_listener = add_listener
+
+            def set_remove_listener(self, remove_listener):
+                self.remove_listener = remove_listener
+
+            def set_pacemaker(self, pacemaker):
+                self.pacemaker = pacemaker
+
+            def enable(self):
+                # Add a high-priority deactivate() listener on every tick
+                self.add_listener("trigger", self.update_triggers, "tick", priority=9)
+
             def place(self):
-                raise NotImplementedError("sparta.triggers.delay has not been implemented yet")
+                self.pending_triggers = []
+                libcontext.plugin(("bee", "init"), plugin_single_required(self.enable))
+
+                libcontext.socket("pacemaker", socket_single_required(self.set_pacemaker))
+                libcontext.socket(("evin", "add_listener"), socket_single_required(self.set_add_listener))
+                libcontext.socket(("evin", "remove_listener"), socket_single_required(self.set_remove_listener))
 
         return delay
