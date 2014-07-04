766556145fb3f5d2591ef714f176f4bb719d7700
agoose77 <goosey15@gmail.com>
2014-07-04 20:38:58 +0100

    Improved mytype documentation

--- bee/mytype.py
+++ bee/mytype.py
@@ -6,35 +6,53 @@ python3 = (sys.version_info[0] == 3)
 if python2:
     mytype = type
     myobject = object
+
 elif python3:
     cache = set()
 
     class mytype(type):
-        def __new__(metacls, name, bases, dic, **kargs):
+
+        """Custom metaclass to handle Python 2 and Python 3 syntax differences.
+
+        Supports __metaclass__ syntax, provided metaclass derives from mytype
+        """
+
+        def __new__(metacls, name, bases, cls_dict, **kargs):
             meta = type
-            remcache = False
+            modified_cache = False
+
             if metacls not in cache:
                 cache.add(metacls)
-                remcache = True
-                if "__metaclass__" in dic:
-                    meta = dic["__metaclass__"]
-                else:
-                    for b in bases:
-                        if hasattr(b, "__metaclass__"):
-                            meta = b.__metaclass__
-                            break
-            if meta is mytype:
-                # print("=MYTYPE!", name)
-                ret = type(name, bases, dic)
-            elif meta is type:
-                # print("=TYPE!", name)
-                ret = type.__new__(metacls, name, bases, dic, **kargs)
-            else:
-                # print("=META",meta,metacls)
-                ret = meta.__new__(meta, name, bases, dic, **kargs)
-            ret.__metaclass__ = meta
-            if remcache: cache.remove(metacls)
-            # print("DONE", name)
-            return ret
+                modified_cache = True
+                if "__metaclass__" in cls_dict:
+                    meta = cls_dict["__metaclass__"]
 
-    exec("class myobject(object,metaclass=mytype):pass")
+                else:
+                    for base_cls in bases:
+                        if hasattr(base_cls, "__metaclass__"):
+                            meta = base_cls.__metaclass__
+                            break
+
+            # Default state, we are taking the place of type
+            if meta is type:
+                returned_cls = super().__new__(metacls, name, bases, cls_dict, **kargs)
+
+            # We allow ourselves to call type
+            elif meta is mytype:
+                returned_cls = type(name, bases, cls_dict)
+
+            # Inherited metaclass must first be called
+            else:
+                returned_cls = meta.__new__(meta, name, bases, cls_dict, **kargs)
+
+            returned_cls.__metaclass__ = meta
+
+            if modified_cache:
+                cache.remove(metacls)
+
+            return returned_cls
+
+
+    class myobject(metaclass=mytype):
+
+        pass
